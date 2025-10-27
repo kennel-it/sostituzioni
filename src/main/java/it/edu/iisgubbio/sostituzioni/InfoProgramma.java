e1bbd09f19d44b03ddb763aa4eff65277a51255d
Edoardo Panfili <edoardo.panfili@iisgubbio.edu.it>
2025-10-27 18:42:52 +0100

    aggiunto Michele Bellucci alle info sul programma, aggiunto plugin maven-exec a pom.xml, versione portata a 4.1.0

--- src/main/java/it/edu/iisgubbio/sostituzioni/InfoProgramma.java
+++ src/main/java/it/edu/iisgubbio/sostituzioni/InfoProgramma.java
@@ -9,10 +9,13 @@ import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
 
 public class InfoProgramma{
-	private String[] sviluppatori = {"Edoardo Panfili","Claudio Giammarioli","Angeloni Pietro","Battistoni Giulia","Bejdo Loris","Biagiotti Luca",
+	private String[] sviluppatori = {"Edoardo Panfili","Claudio Giammarioli",
+			"Michele Bellucci", // 2025-26
+			"Diego Mariucci",   // 2023-24
+			"Luzi Davide","Andrei Riginel Ungureanu","Flavio Merli","Omar Mohamed Nasr", // 2021-22
+			"Angeloni Pietro","Battistoni Giulia","Bejdo Loris","Biagiotti Luca",
 			"Casagrande Nicola","Cenni Gabriele","Costea Bianca Ioana","Ermirandi Jennifer","Kakcuku Sokol","Mancini Giada",
-			"Morelli Cristian","Natalini Sofia","Pierotti Giorgio","Rinaldoni Matteo","Vantaggi Leonardo","Vantaggi Michele",
-			"Luzi Davide","Andrei Riginel Ungureanu","Flavio Merli","Omar Mohamed Nasr","Diego Mariucci"};
+			"Morelli Cristian","Natalini Sofia","Pierotti Giorgio","Rinaldoni Matteo","Vantaggi Leonardo","Vantaggi Michele"};
 	@FXML
 	Label lController;
 	@FXML
@@ -27,16 +30,16 @@ public class InfoProgramma{
     	lController.setText(javaVersion);
     	lVersione.setText(Ambiente.VERSIONE);
     	lAggiornamento.setText(Ambiente.DATA_COMPILAZIONE);
-    	
+
     	Random rand = new Random();
-		
+
 		for (int i = 0; i < sviluppatori.length; i++) {
 			int randomIndexToSwap = rand.nextInt(sviluppatori.length);
 			String temp = sviluppatori[randomIndexToSwap];
 			sviluppatori[randomIndexToSwap] = sviluppatori[i];
 			sviluppatori[i] = temp;
 		}
-		
+
 		for (int i = 0; i < sviluppatori.length; i++) {
 			String stringa = sviluppatori[i];
 			lListaSviluppatori.getItems().add(stringa);
